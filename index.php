bb88bdbbd13d4484cf1899ce9c37311b915eb0c4
LaurentArcos <laurent.arcos@gmail.com>
2023-04-06 16:39:22 +0200

    correction des bugs avec isset

--- index.php
+++ index.php
@@ -2,20 +2,17 @@
 $aDeviner = 150;
 ?>
 
-<pre>
-  <?php var_dump($_GET) ?>
-</pre>
-
-<?php if ($_GET['chiffre'] > $aDeviner) : ?>
-  Votre chiffre est trop grand ! 
-<?php elseif ($_GET['chiffre'] < $aDeviner) : ?>
-  Votre chiffre est trop petit !    
-<?php else: ?>
-  Bravo ! Vous avez deviné le chiffre <?= $aDeviner ?>
+<?php if (isset($_GET['chiffre'])):?>
+  <?php if ($_GET['chiffre'] > $aDeviner) : ?>
+    Votre chiffre est trop grand ! 
+  <?php elseif ($_GET['chiffre'] < $aDeviner) : ?>
+    Votre chiffre est trop petit !    
+  <?php else: ?>
+    Bravo ! Vous avez deviné le chiffre <?= $aDeviner ?>
+  <?php endif ?>
 <?php endif ?>
 
-
 <form action="/index.php" method="GET">
-  <input type="number" name="chiffre" placeholder="entre 0 et 1000" value="<?=htmlentities($_GET['chiffre'])?>">
+  <input type="number" name="chiffre" placeholder="entre 0 et 1000" value="<?php if (isset($_GET['chiffre'])) { echo htmlentities($_GET['chiffre']); } ?>">
   <button type="submit">Deviner</button>
 </form>
