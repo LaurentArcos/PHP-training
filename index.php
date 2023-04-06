7ba9ec8045d5cfaee03d1d4bff81f6eb571ed583
LaurentArcos <laurent.arcos@gmail.com>
2023-04-06 16:49:05 +0200

    refactoring

--- index.php
+++ index.php
@@ -1,18 +1,35 @@
 <?php
 $aDeviner = 150;
+
+$erreur = null;
+$succes = null;
+$value = null;
+
+if (isset($_GET['chiffre'])) {
+    if ($_GET['chiffre'] > $aDeviner) {
+      $erreur = "Votre chiffre est trop grand !";
+    } elseif ($_GET['chiffre'] < $aDeviner) {
+      $erreur = "Votre chiffre est trop petit !";
+    } else {
+      $succes = "Bravo ! vous avez deviné le nombre <strong>$aDeviner</strong>";
+    }
+    $value = (int)$_GET['chiffre'];
+}
+
 ?>
 
-<?php if (isset($_GET['chiffre'])):?>
-  <?php if ($_GET['chiffre'] > $aDeviner) : ?>
-    Votre chiffre est trop grand ! 
-  <?php elseif ($_GET['chiffre'] < $aDeviner) : ?>
-    Votre chiffre est trop petit !    
-  <?php else: ?>
-    Bravo ! Vous avez deviné le chiffre <?= $aDeviner ?>
-  <?php endif ?>
+<?php if ($erreur): ?>
+  <div>
+    <?= $erreur ?>
+  </div>
+  <?php elseif ($succes):?>
+  <div>
+    <?= $succes ?>
+  </div>
 <?php endif ?>
 
+
 <form action="/index.php" method="GET">
-  <input type="number" name="chiffre" placeholder="entre 0 et 1000" value="<?php if (isset($_GET['chiffre'])) { echo htmlentities($_GET['chiffre']); } ?>">
+  <input type="number" name="chiffre" placeholder="entre 0 et 1000" value="<?php $value ?>">
   <button type="submit">Deviner</button>
 </form>
