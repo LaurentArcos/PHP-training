dd55e0b3b80c56147be3da66b624fee845531546
LaurentArcos <laurent.arcos@gmail.com>
2023-04-12 16:18:17 +0200

    compteur de vues

--- CompteurDeVisites/functions.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-function ajouter_vue () {
-
-  
-}
--- test.php
+++ test.php
@@ -1,6 +1,10 @@
 <?php
 
-file_put_contents('demo.txt','Salut les gens', FILE_APPEND);
+// file_put_contents('demo.txt','Salut les gens', FILE_APPEND);
 
-file_get_contents('demo2.txt');
+// file_get_contents('demo2.txt');
+
+require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . "CompteurDeVisites" . DIRECTORY_SEPARATOR . 'compteur.php';
+
+ajouter_vue();
 
